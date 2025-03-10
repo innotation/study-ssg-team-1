2de84a5a240ba35a480f7babbf2120b21d7bb238
Kyungbae_Go <gokyungbae@gmail.com>
2025-03-11 00:27:53 +0900

    1. 콜라문제

---
+++ kkb/week2/src/main/java/com/kyungbae/practice1/Solution.java
@@ -0,0 +1,20 @@
+package com.kyungbae.practice1;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 1;
+        int n = 20;
+
+        int count = 0;
+        int less = 0;
+        while (n >= 1){
+            n = n+less;
+            less = (n%a);
+            n = (n/a)*b;
+            count += n;
+        }
+        System.out.println(count);
+    }
+}
