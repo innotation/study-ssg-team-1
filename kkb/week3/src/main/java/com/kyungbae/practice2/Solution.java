63f99cf78866829cd90371d05aa18c985c1a4aac
Kyungbae_Go <gokyungbae@gmail.com>
2025-03-18 23:22:02 +0900

    Solve : 팰린드롬 만들기

--- kkb/week3/src/main/java/com/kyungbae/practice2/Solution.java
+++ kkb/week3/src/main/java/com/kyungbae/practice2/Solution.java
@@ -7,14 +7,12 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
-        // System.out.println(palindrom(str));
-        for (int i = 0; i < str.length(); i++) {
-            System.out.println(str.toCharArray());
-        }
+         System.out.println(palindrom(str));
 
     }
 
     public static String palindrom(String str){
+
         String result = "I'm Sorry Hansoo";
 
         /*
@@ -23,12 +21,49 @@ public class Solution {
             알파벳 순서대로 반으로 나누어 양쪽끝에 배치
             홀수개 있는 알파벳 1개는 가운데 배치
          */
+
+        // 알파벳 요소별로 묶기
         Map<Character, Integer> alpha = new HashMap<>();
         for (int i = 0; i <str.length() ; i++) {
-            alpha.put(str.charAt(i), i);
+            alpha.put(str.charAt(i), alpha.getOrDefault(str.charAt(i), 0) + 1);
         }
 
+        // 홀수 개수 확인
+        int oddCount = 0;
+        char oddChar = ' ';
+        for (Map.Entry<Character, Integer> count : alpha.entrySet()){
+            if (count.getValue() % 2 != 0) {
+                oddCount++;
+                oddChar = count.getKey();
+            }
+        }
 
+        // 홀수가 2개 이상이면 종료
+        if (oddCount > 1) {
+            return result;
+        }
+
+        // 알파벳 순서대로 정렬하기
+        List<Character> sortAlpha = new ArrayList<>(alpha.keySet());
+        Collections.sort(sortAlpha);
+
+        // 문자 합치기
+        StringBuilder leftChar = new StringBuilder();
+        StringBuilder rightChar = new StringBuilder();
+        StringBuilder cenChar = new StringBuilder();
+
+        for (char c : sortAlpha) {
+            int count = alpha.get(c);
+            String r = String.valueOf(c).repeat(count/2);
+            leftChar.append(r);
+            rightChar.insert(0, r);
+
+            if (count % 2 != 0) {
+                cenChar.append(c);
+            }
+        }
+
+        result = leftChar.toString() + cenChar.toString() + rightChar.toString();
 
         return result;
     }
