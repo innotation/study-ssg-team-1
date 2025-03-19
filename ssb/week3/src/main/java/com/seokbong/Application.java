b95b0dbccaf1561d0dd986a2d339daa39ee34ad8
seokbong <ssb7779@naver.com>
2025-03-19 19:17:58 +0900

    Create.Problem3.java

---
+++ ssb/week3/src/main/java/com/seokbong/Application.java
@@ -0,0 +1,10 @@
+package com.seokbong;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Problem3 p = new Problem3();
+
+        System.out.println(p.solution(11));
+    }
+}
